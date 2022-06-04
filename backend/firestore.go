e544b154799b9b7ace9421dcc3ddef66bd671ed0
Louie <59896415+eliasdandouch@users.noreply.github.com>
2022-06-04 12:03:19 -0700

    added gotiming functions to api calls & the database

--- backend/firestore.go
+++ backend/firestore.go
@@ -8,27 +8,25 @@ import (
 
 	"google.golang.org/api/option"
 )
-  
+
 func insertSong(songData Song) {
 	go func() {
-		opt := option.WithCredentialsFile("./da-community-bot-firebase-adminsdk-1lkpq-2f93bb5073.json")	
+		opt := option.WithCredentialsFile("./da-community-bot-firebase-adminsdk-1lkpq-8121942415.json")
 		app, err := firebase.NewApp(context.Background(), nil, opt)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		client, err := app.Firestore(context.Background())
 		if err != nil {
-		  log.Fatalln(err)
+			log.Fatalln(err)
 		}
-	
-			
+
 		ref := client.Collection("chenzo-song-request").NewDoc()
-	
+
 		result, err := ref.Set(context.Background(), songData)
-		if(err != nil) {
+		if err != nil {
 			log.Fatalln(err)
 		}
 		log.Print(result)
 	}()
 }
-  
