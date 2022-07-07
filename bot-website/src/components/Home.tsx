955f289ee2dfc5247ea0bc53dfa12c196702919f
Elias Dandouch <louie@Eliass-Mac-Studio.local>
2022-07-07 01:34:34 -0700

    added a responsive home nav bar component

--- bot-website/src/components/Home.tsx
+++ bot-website/src/components/Home.tsx
@@ -1,10 +1,8 @@
-import { Button, Box } from '@mui/material';
+
 import axios from 'axios';
 import * as React from 'react'
 import { Auth } from '../auth/auth';
-import { AuthButton } from './auth/AuthButton';
-import ButtonAppBar from './ButtonAppBar';
-import { MUIMenu } from './mui/Menu';
+import { HomeNavBar } from './HomeNavBar';
 
 export const Home: React.FC = () => {
   const [authenticated, setAuthenticated] = React.useState(false);
@@ -16,27 +14,14 @@ export const Home: React.FC = () => {
     })
   }, [])
 
+
   return (
-  // // <Box sx={{flexGrow: 1}}>
-  //  <div className='mx-auto max-w-[865px] w-full p-8'>
-  //    <div className='justify-between items-center'>
-  //     <nav className='bg-[#091126] w-auto h-auto rounded-[25px] mt-[35px]'>
-  //       <div className='mx-auto flex flex-row w-full'>
-  //       <div className='mt-[24px] ml-10 font-semibold text-[20px] font-roboto'>DaCommunityBot</div>
-  //       <div className='flex w-full'>{ !authenticated ? <Button variant="contained" className='flex-shrink-1 mx-auto px-auto ml-[30rem] bg-[#121E40] mt-5 bg-opacity-50 font-bold rounded-2xl border border-solid border-[#343333] w-[100px] h-[36px]'>Login</Button> : <Button>Dashboard</Button> }</div>
-  //       </div>
-  //     </nav>
-  //    </div>
-  //  </div>
-  // //  </Box>
-  // // <ButtonAppBar/>
-  <nav className="bg-[#091126] px-2 sm:px-4 py-2.5 rounded-2xl max-w-[685px] w-full mx-auto mt-4">
-  <div className="container flex flex-row justify-between items-center mx-auto">
-    <span className="flex items-center">
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">DaCommunityBot</span>
-    <div>{ !authenticated ? <Button variant="contained" className='mx-auto px-auto ml-[24rem] bg-[#121E40] mt-3 bg-opacity-50 font-bold rounded-2xl border border-solid whitespace-nowrap border-[#343333]'>Login</Button> : <Button>Dashboard</Button> }</div>
-    </span>
-  </div>
-</nav>
+    <div>
+      <HomeNavBar/>
+      <div className='flex justify-center items-center flex-wrap w-full'>
+        <span className='hidden md:block mr-[395px] mt-8 font-semibold text-[58px] whitespace-pre-line w-[439px] h-[364px] leading-[96px]'>Free, Simple and easy to use Twitch Bot catered to DaCommunity.</span>
+      </div>
+    </div>
   )
 }
+/* { !authenticated ? <Button variant="contained" classNameName='ml-[24rem] bg-[#121E40] mt-3 bg-opacity-50 font-bold rounded-2xl border border-solid whitespace-nowrap border-[#343333]'>Login</Button> : <Button>Dashboard</Button> } */
---
+++ bot-website/src/components/HomeNavBar.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Twitch, Discord, Github } from 'react-bootstrap-icons';
+
+export const HomeNavBar: React.FC = () => {
+    return (
+        <nav className="bg-[#091126] px-2 sm:px-4 py-2.5 rounded-[25px] lg:w-[835px] h-[75px] w-full mx-auto mt-4 flex flex-1">
+        <div className="container flex flex-row justify-between items-center mx-auto">
+          <div className="flex flex-1 items-center">               
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">DaCommunityBot</span>
+          </div>
+            <div className='hidden w-full md:block md:w-auto items-center ml-[28rem]'>
+              <div className='flex flex-1 -ml-20'>
+                <Twitch size={22}/>
+                <Discord size={22} className='ml-4'/>
+                <Github size={22} className='ml-4'/>
+              </div> 
+            </div>
+        </div>
+      </nav>
+    );
+}
