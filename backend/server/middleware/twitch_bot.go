d62ba9bebdc4f41008c59e4f46f54c967be0f259
Louie <eliasdandouch@gmail.com>
2026-03-02 14:40:26 -0800

    added a proper heartbeat mechanism to stay alive (WS)

--- backend/server/middleware/twitch_bot.go
+++ backend/server/middleware/twitch_bot.go
@@ -130,5 +130,8 @@ func GetAllJoinedTwitchChannels(c *fiber.Ctx) error {
 			"error": err.Error(),
 		})
 	}
-	return c.Status()
+	return c.Status(200).JSON(fiber.Map{
+		"status":   "success",
+		"channels": channels,
+	})
 }
